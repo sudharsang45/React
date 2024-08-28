a8a6a363bd51404673660f26f998deed6e127d69
Sudharsan Ganesan <sudharsang45@protonmail.com>
2024-08-28 00:02:44 -0400

    replaced class components with hooks in Add developer

--- src/AddDeveloper.js
+++ src/AddDeveloper.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import Developer from './Developer';
 
-class AddDeveloper extends React.Component {
-    constructor(props){
+function AddDeveloper () {
+   /* constructor(props){
         super(props);
         this.state = {
             firstName: '',
@@ -20,20 +21,28 @@ class AddDeveloper extends React.Component {
         this.setState({
             [name]: value
         })
-    }   
+    }   */
+    const navigate = useNavigate();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [favoriteLanguage, setFavoriteLanguage] = useState('');
+    const [yearStarted, setYearStarted] = useState(null);
 
-    clearForm = () => {
-        this.setState({firstName: '', lastName: '', favouriteLanguage: '', yearStarted: null});
+    /*const clearForm = () => {
+        setFirstName('');
+        setLastName('');
+        setFavoriteLanguage('');
+        setYearStarted(null);
         document.getElementById('devForm').reset();
-    }
+    }*/
 
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
         /*this.props.handleNewDeveloper(new Developer(null, this.state.firstName, this.state.lastName,
                                         this.state.favouriteLanguage, this.state.yearStarted)
         );*/
 
-        let newDev = new Developer(null, this.state.firstName, this.state.lastName,this.state.favoriteLanguage, this.state.yearStarted);
+        let newDev = new Developer(null, firstName, lastName, favoriteLanguage, yearStarted);
 
         fetch('https://dev-bios-api-dot-cog01hprmn542jqme4w772bk1dxpr.uc.r.appspot.com/developers/add', {
             method: 'POST',
@@ -43,42 +52,40 @@ class AddDeveloper extends React.Component {
             body: JSON.stringify(newDev)    
             }
         )  
-        .then(() => this.clearForm())
+        .then(() => navigate('/developers'))
         .catch(err => console.log(err));            
     }
 
-    render(){
-        return  (
-            <div className="container">
-                <h1> ADD Developer Bio</h1>
-                <div className="row">
-                    <div className="col-mid-6">
-                        <form id="devForm" onSubmit={ this.handleSubmit }>
-                            <div className="form-group">
-                                <label htmlFor="firstName">First Name</label>
-                                <input type="text" data-testid="firstName" name="firstName"  className="form-control" onChange={this.handleChange } />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="lastName">Last Name</label>
-                                <input type="text" data-testid="lastName" name="lastName"  className="form-control" onChange={this.handleChange} />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="favoriteLanguage">Favorite Language</label>
-                                <input type="text" data-testid="favoriteLanguage" name="favoriteLanguage"  className="form-control" onChange={this.handleChange} />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="yearStarted">Year Started</label>
-                                <input type="text" data-testid="yearStarted" name="yearStarted"  className="form-control" onChange={this.handleChange  } />
-                            </div>
-                            <div className="form-group">
-                                <button type="submit"  className="btn btn-success" >Submit</button>
-                            </div>
-                        </form>
-                    </div>
+    return  (
+        <div className="container">
+            <h1> ADD Developer Bio</h1>
+            <div className="row">
+                <div className="col-mid-6">
+                    <form id="devForm" onSubmit={ handleSubmit }>
+                        <div className="form-group">
+                            <label htmlFor="firstName">First Name</label>
+                            <input type="text" data-testid="firstName" name="firstName"  className="form-control" onChange={ (event) => setFirstName(event.target.value) } />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="lastName">Last Name</label>
+                            <input type="text" data-testid="lastName" name="lastName"  className="form-control" onChange={ (event) => setLastName(event.target.value) } />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="favoriteLanguage">Favorite Language</label>
+                            <input type="text" data-testid="favoriteLanguage" name="favoriteLanguage"  className="form-control" onChange={ (event) => setFavoriteLanguage(event.target.value) } />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="yearStarted">Year Started</label>
+                            <input type="text" data-testid="yearStarted" name="yearStarted"  className="form-control" onChange={ (event) => setYearStarted(event.target.value) } />
+                        </div>
+                        <div className="form-group">
+                            <button type="submit"  className="btn btn-success" >Submit</button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
     
 }
 
